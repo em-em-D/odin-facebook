c4871785f91aedf8216188e144d60d5109b66914
em-em-D <michaeldamulak@gmail.com>
2019-08-19 12:10:01 +0100

    user and post model created devise included all associations and test working efficiently

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+        
+ has_many :posts, dependent: :destroy
+end
